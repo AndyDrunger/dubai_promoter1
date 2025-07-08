9d39d5b5084981cc5fc5165f208bd2e9b02202ed
AndyDrunger <x666031666@gmail.com>
2025-07-08 11:14:30 +0400

    ttl fix

--- rabbitmq/producer/main.py
+++ rabbitmq/producer/main.py
@@ -15,12 +15,11 @@ from aio_pika.abc import AbstractRobustExchange as Exchange
 
 
 async def publish_msg(exchange: Exchange, payload: dict, routing_key: str, ttl_sec: int | None = None):
-    expiration = ttl_sec if ttl_sec is not None else None
 
     message = aio_pika.Message(
         body=pickle.dumps(payload),
         delivery_mode=aio_pika.DeliveryMode.PERSISTENT,
-        expiration=expiration  # TTL в миллисекундах
+        expiration=ttl_sec
     )
 
     await exchange.publish(
